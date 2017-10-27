203feb37a0adeabe17ac2dd7d424fb2c63d7100c
wlopez <wilmer.lopez@outlook.com>
2017-10-27 09:38:22 -0500

    Enter

--- routers/router.go
+++ routers/router.go
@@ -2,9 +2,11 @@ package routers
 
 import (
 	"webApp01/controllers"
+
 	"github.com/astaxie/beego"
 )
 
 func init() {
-    beego.Router("/", &controllers.MainController{})
+	beego.Router("/", &controllers.MainController{})
+	beego.Router("/home", &controllers.HomeController{})
 }
